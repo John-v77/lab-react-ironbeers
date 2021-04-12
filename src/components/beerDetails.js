dcaa8dec31aee3b40f9485ee1b7b16639634d28f
John Vasile <johny_tnt@yahoo.com>
2021-04-12 18:40:30 -0400

    beer detail Style

--- src/components/beerDetails.js
+++ src/components/beerDetails.js
@@ -1,30 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import './styles/beerDetails.css';
 
 function BeerDetails(props) {
-    console.log(props)
-    // console.log(props.match.params.id)
+
     let [beer, setBeer] = useState([])
 
-    // /${props.match.params.id}
 useEffect(() => {
     axios.get(`https://ih-beers-api2.herokuapp.com/beers/${props.match.params.id}`)
     .then(dataBeer1 =>{
-        // console.log(dataBeer1)
         setBeer(dataBeer1.data)})
-    
     }, [])
 
-    // console.log(props.match)
     return (
-        <div style={{width:'50vw', backgroundColor:'white', marginLeft:'25vw', paddingTop:'80px', minHeight:'100vh'}}>
-            <img style={{height:'200px', width:'120px'}} src={beer.image_url} alt=""/>
+        <div className='beer-details'>
+            <img className='beer-details-img' src={beer.image_url} alt=""/>
             <h2>{beer.name}</h2>
             <p>{beer.tagline}</p>
-            <p>{beer.first_brewed}</p>
-            <p>{beer.attenuation_level}</p>
-            <p>{beer.description}</p>
-            <p>{beer.contributed_by}</p>
+            <p>first brewed: {beer.first_brewed}</p>
+            <p>Attenuation Level: {beer.attenuation_level}</p>
+            <p className='Beer-description'>{beer.description}</p>
+            <p>contributed by: {beer.contributed_by}</p>
         </div>
     );
 }
